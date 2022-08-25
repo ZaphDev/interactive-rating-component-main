144b4dd6c9b5d6f95deea5b3d164849dd8319de6
Anuar Zamudio <anuarzamudio11@gmail.com>
2022-08-25 18:07:29 -0500

    First Js tests succesfully

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 var rating="0";
-document.getElementById("demo").innerHTML = rating;
+var ratingSelected=true;
+
+document.getElementById("rating").innerHTML = rating;
+function submitRating(){
+    if(ratingSelected == true){
+    document.getElementById('card1Switch').style.display="none";
+    document.getElementById('card2Switch').style.display="flex";
+    }
+    else {
+        
+    }
+}
